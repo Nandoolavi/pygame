3a47bae21fb30c4c59c6056dc8a35221bdff8cc9
FernandoDavi <fernandovieri.lavieri@gmail.com>
2024-08-15 16:01:53 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,118 @@
+#print  ("Hello world")
+#nome = 'Fernando'
+#tipo = 'humano'
+
+#print(type(nome))
+#print(type(tipo))
+
+
+#x = 2
+#y = 3
+#print (type(x + y))
+
+#altura = 1.78
+#peso = 60.1
+#
+#print(type(peso))
+#print(type(altura))
+
+#kyofoda = True
+#murilohetero = False
+#
+#print(type(kyofoda))
+#print(type(murilohetero))
+
+#a = 5+2j
+#b = 20+6j
+#
+#print(type(a))
+#print(type(b))
+#print(complex(2, 5))
+
+#alunos = ['Amanda', 'Ana', 'Bruno', 'João']
+#notas = [10, 8, 7.8, -1]
+
+#print(type(alunos))
+#print(type(notas))
+
+#valores = (90, 78, 79, 80, 65)
+#pontos = (100, 94.05, 86.08, 73)
+
+#print(type(valores))
+#print(type(pontos))
+
+#altura = {'pedro':3.141516171881920}
+#peso = {'pedro': 90}
+
+#print(type(altura))
+#print(type(peso))
+
+#x = {1, 3, 5}
+#y = {'joao' 'cafe' 'nanana'}
+#print(type(x))
+#print(type(y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
