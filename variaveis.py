db8461ffd876ac63c1259e24e6665e166bb2ec4d
FernandoDavi <fernandovieri.lavieri@gmail.com>
2024-09-19 16:03:03 -0300

    Update variaveis.py

--- variaveis.py
+++ variaveis.py
@@ -61,14 +61,107 @@
 #
 #tabuada()
 
-class animal:
-    def __init__ (self, filo, cor, sexo):
-        self.filo = filo
-        self.cor = cor
-        self.sexo = sexo
+#class animal:
+#    def __init__ (self, filo, cor, sexo):
+#        self.filo = filo
+#        self.cor = cor
+#        self.sexo = sexo
+#
+#    def estudar(self):
+#        print (f'seu animal é um {self.filo} de cor {self.cor} e de sexo {self.sexo}')
+#
+#raposa = animal('felino', 'preta', 'masculino')
+#raposa.estudar()
 
-    def estudar(self):
-        print (f'seu animal é um {self.filo} de cor {self.cor} e de sexo {self.sexo}')
+#import pygame
+#from pygame.Locals import *
+#
+#pygame.init()
+#screen = pygame.display.set_mode((600, 600))
+#pygame.display.set_caption('Snake')
+#
+#snake = [(200, 200), (210, 200), (220,200)]
+#snake_skin = pygame.Surface((10,10))
+#snake_skin.fill((255,255,255)) #Branco
+#
+#while True:
+#  for event in pygame.event.get():
+#    if event.type == QUIT:
+#      pygame.quit()
+#      exit()
+#  screen.fill((0,0,0))
+#  for pos in snake:
+#    screen.blit(snake_skin,pos)
+#  pygame.display.update()
+#  
+#  UP = 0
+#  RIGHT = 1
+#  DOWN = 2
+#  LEFT = 3
+#  
+#  pygame.init()
+#  screen = pygame.display.set_mode((600,600))
+#  pygame_skin.fill((255,255,255))
+#  
+#  my_direction = LEFT
+#  
+#  while True:
+#    for event in pygame.event.get():
+#      if event.type == QUIT:
+#        pygame.quit()
+#        exit()
+#        
+#    if event.type == KEYDOWN:
+#      
+#      if event.key == K_UP: my_direction = UP
+#      if event.key == K_DOWN: my_direction = DOWN
+#      if event.key == K_LEFT: my_direction = LEFT
+#      if event.key == K_RIGHT: my_direction = RIGHT
+#      
+#for i in range(len(snake) - 1, 0, -1):
+#  snake[i] = (snake[i-1][0], snake[i-1][1])
+#  
+#if my_direction == UP:
+#    snake[0] = (snake[0][0], snake[0][1] - 10)
+#import pygame
+#pygame.init()
+#
+#screen = pygame.display.set_mode((1000,1000))
+#pygame.display.set_caption("Eventos")
+#
+#running = True
+#while running:
+#    for event in pygame.event.get():
+#        print(event)
+#        if event.type == pygame.QUIT:
+#            running = False
 
-raposa = animal('felino', 'preta', 'masculino')
-raposa.estudar()
+from pygame.locals import *
+import pygame 
+
+WIDTH = 500
+HEIGHT = 400
+FPS = 60
+
+BLACK = (13, 13, 13)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Título game")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    sceen.fill(BLACK)
+    pygame.display.flip()
+
+pygame.quit()
