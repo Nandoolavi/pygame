4fc56a58440800ece456ae8834821668e2acc42a
FernandoDavi <fernandovieri.lavieri@gmail.com>
2024-10-10 15:02:02 -0300

    ;

--- variaveis.py
+++ variaveis.py
@@ -179,73 +179,73 @@
 #from math import pi
 
 
-from dataclasses import dataclass
-import pygame as pg
-pg.init()
-
-RED = (255, 0, 0)
-YELLOW = (0, 255, 255)
-WHITE = (255, 255, 255)
-BLUE= (0, 0, 255)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-
-width = 450
-height = 200
-screen = pg.display.set_mode((width, height))
-
-clock = pg.time.Clock()
-pg.screen = pg.display.set_mode
-(screen, True, 32)
-
-player_image = pg.image.load('player.png').convert_alpha()
-player = pg.transform.scale(player_image, (50, 75))
-
-moving_right = False
-moving_left = False
-@dataclass
-class PlayerLocation:
-    x: int 
-    y: int
-
-player_location = PlayerLocation(x=155, y=125)
-velocity = 3.5
-FPS = 60
-running = True
-while running:
-    screen.fill(BLACK)
-    screen.blit(player,(player_location.x, player_location.y))
-    
-    if moving_right:
-        player_location.x += velocity
-    if moving_left:
-        player_location.x -= velocity        
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_RIGHT:
-               moving_left = True
-            if event.key == pg.K_LEFT:
-                moving_right = True
-
-        if event.type == pg.KEYUP:
-            if event.key == pg.K_RIGHT:
-                moving_right = False
-            if event.key == pg.K_LEFT:
-                moving_left = False
-    
-
-    if player_location.x < 0:
-        player_location.x = 0
-    if player_location.x + player.get_width() > width:
-        player_location.x = width - player.get_width()
-
-    pg.display.update()
-    clock.tick(FPS)
-
-pg.quit()
+#from dataclasses import dataclass
+#import pygame as pg
+#pg.init()
+#
+#RED = (255, 0, 0)
+#YELLOW = (0, 255, 255)
+#WHITE = (255, 255, 255)
+#BLUE= (0, 0, 255)
+#GREEN = (0, 255, 0)
+#BLACK = (0, 0, 0)
+#
+#width = 450
+#height = 200
+#screen = pg.display.set_mode((width, height))
+#
+#clock = pg.time.Clock()
+#pg.screen = pg.display.set_mode
+#(screen, True, 32)
+#
+#player_image = pg.image.load('player.png').convert_alpha()
+#player = pg.transform.scale(player_image, (50, 75))
+#
+#moving_right = False
+#moving_left = False
+#@dataclass
+#class PlayerLocation:
+#    x: int 
+#    y: int
+#
+#player_location = PlayerLocation(x=155, y=125)
+#velocity = 3.5
+#FPS = 60
+#running = True
+#while running:
+#    screen.fill(BLACK)
+#    screen.blit(player,(player_location.x, player_location.y))
+#    
+#    if moving_right:
+#        player_location.x += velocity
+#    if moving_left:
+#        player_location.x -= velocity        
+#    for event in pg.event.get():
+#        if event.type == pg.QUIT:
+#            running = False
+#
+#        if event.type == pg.KEYDOWN:
+#            if event.key == pg.K_RIGHT:
+#               moving_left = True
+#            if event.key == pg.K_LEFT:
+#                moving_right = True
+#
+#        if event.type == pg.KEYUP:
+#            if event.key == pg.K_RIGHT:
+#                moving_right = False
+#            if event.key == pg.K_LEFT:
+#                moving_left = False
+#    
+#
+#    if player_location.x < 0:
+#        player_location.x = 0
+#    if player_location.x + player.get_width() > width:
+#        player_location.x = width - player.get_width()
+#
+#    pg.display.update()
+#    clock.tick(FPS)
+#
+#pg.quit()
 
 
 #retangulos = [
@@ -275,3 +275,123 @@ pg.quit()
 #    pg.draw.polygon(screen, YELLOW, [[140, 120], [100, 200], [300, 200]])
 #    pg.draw.polygon(screen, GREEN, [[140, 120], [100, 200], [300,200]])
 #    pg.display.update()
+
+##### 10 - Game over ####
+import pygame, random
+from pygame.locals import *
+
+# Helper functions
+def on_grid_random():
+    x = random.randint(0,59)
+    y = random.randint(0,59)
+    return (x * 10, y * 10)
+
+def collision(c1, c2):
+    return (c1[0] == c2[0]) and (c1[1] == c2[1])
+
+# Macro definition for snake movement.
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Snake')
+
+snake = [(200, 200), (210, 200), (220,200)]
+snake_skin = pygame.Surface((10,10))
+snake_skin.fill((255,255,255)) #White
+
+apple_pos = on_grid_random()
+apple = pygame.Surface((10,10))
+apple.fill((255,0,0))
+
+my_direction = LEFT
+
+clock = pygame.time.Clock()
+
+font = pygame.font.Font('freesansbold.ttf', 18)
+score = 0
+
+game_over = False
+while not game_over:
+    clock.tick(10)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+        if event.type == KEYDOWN:
+            if event.key == K_UP and my_direction != DOWN:
+                my_direction = UP
+            if event.key == K_DOWN and my_direction != UP:
+                my_direction = DOWN
+            if event.key == K_LEFT and my_direction != RIGHT:
+                my_direction = LEFT
+            if event.key == K_RIGHT and my_direction != LEFT:
+                my_direction = RIGHT
+
+    if collision(snake[0], apple_pos):
+        apple_pos = on_grid_random()
+        snake.append((0,0))
+        score = score + 1
+        
+    # Check if snake collided with boundaries
+    if snake[0][0] == 600 or snake[0][1] == 600 or snake[0][0] < 0 or snake[0][1] < 0:
+        game_over = True
+        break
+    
+    # Check if the snake has hit itself
+    for i in range(1, len(snake) - 1):
+        if snake[0][0] == snake[i][0] and snake[0][1] == snake[i][1]:
+            game_over = True
+            break
+
+    if game_over:
+        break
+    
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])
+        
+    # Actually make the snake move.
+    if my_direction == UP:
+        snake[0] = (snake[0][0], snake[0][1] - 10)
+    if my_direction == DOWN:
+        snake[0] = (snake[0][0], snake[0][1] + 10)
+    if my_direction == RIGHT:
+        snake[0] = (snake[0][0] + 10, snake[0][1])
+    if my_direction == LEFT:
+        snake[0] = (snake[0][0] - 10, snake[0][1])
+    
+    screen.fill((0,0,0))
+    screen.blit(apple, apple_pos)
+    
+    for x in range(0, 600, 10): # Draw vertical lines
+        pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, 600))
+    for y in range(0, 600, 10): # Draw vertical lines
+        pygame.draw.line(screen, (40, 40, 40), (0, y), (600, y))
+    
+    score_font = font.render('Score: %s' % (score), True, (255, 255, 255))
+    score_rect = score_font.get_rect()
+    score_rect.topleft = (600 - 120, 10)
+    screen.blit(score_font, score_rect)
+    
+    for pos in snake:
+        screen.blit(snake_skin,pos)
+
+    pygame.display.update()
+
+while True:
+    game_over_font = pygame.font.Font('freesansbold.ttf', 75)
+    game_over_screen = game_over_font.render('Game Over', True, (255, 255, 255))
+    game_over_rect = game_over_screen.get_rect()
+    game_over_rect.midtop = (600 / 2, 10)
+    screen.blit(game_over_screen, game_over_rect)
+    pygame.display.update()
+    pygame.time.wait(500)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
